11ce43476f6f030c7dd90565266bbfeee98c6ef4
lorqor <lorqor@gmail.com>
2021-12-06 14:31:44 +0800

    Reconcile EKS clusters and node groups.
    The commit is slightly modified from commit d912a8b5756034904a2c7384d2949ac26180957a.
    Changes:
    - AWS controller now reconciles EKS clusters and node groups.
    - AWS controller now recognizes assume-role and external-id from provider configs.

--- pkg/clients/aws.go
+++ pkg/clients/aws.go
@@ -100,6 +100,39 @@ func UseProviderConfig(ctx context.Context, c client.Client, mg resource.Managed
 	switch s := pc.Spec.Credentials.Source; s { //nolint:exhaustive
 	case xpv1.CredentialsSourceInjectedIdentity:
 		cfg, err := UsePodServiceAccount(ctx, []byte{}, DefaultSection, region)
+		if err != nil {
+			return nil, err
+		}
+		if role, ok := pc.Annotations["assume-role"]; ok {
+			externalId, ok := pc.Annotations["external-id"]
+			if !ok {
+				return nil, errors.New("external-id must be set when assume-role is set")
+			}
+			stsSvc := sts.New(*cfg)
+			sess := strconv.FormatInt(time.Now().UnixNano(), 10)
+			resp, err := stsSvc.AssumeRoleRequest(&sts.AssumeRoleInput{
+				ExternalId:      &externalId,
+				RoleArn:         &role,
+				RoleSessionName: &sess,
+			}).Send(ctx)
+			if err != nil {
+				return nil, err
+			}
+			creds := aws.Credentials{
+				AccessKeyID:     aws.StringValue(resp.Credentials.AccessKeyId),
+				SecretAccessKey: aws.StringValue(resp.Credentials.SecretAccessKey),
+				SessionToken:    aws.StringValue(resp.Credentials.SessionToken),
+			}
+			configs := external.Configs([]external.Config{external.SharedConfig{
+				Credentials: creds,
+				Region:      region,
+			}})
+			config, err := configs.ResolveAWSConfig(external.DefaultAWSConfigResolvers)
+			if err != nil {
+				return nil, err
+			}
+			return SetResolver(ctx, mg, &config), err
+		}
 		return SetResolver(ctx, mg, cfg), err
 	default:
 		data, err := resource.CommonCredentialExtractor(ctx, s, c, pc.Spec.Credentials.CommonCredentialSelectors)
@@ -283,6 +316,33 @@ func GetConfigV1(ctx context.Context, c client.Client, mg resource.Managed, regi
 	}
 	switch s := pc.Spec.Credentials.Source; s { //nolint:exhaustive
 	case xpv1.CredentialsSourceInjectedIdentity:
+		if role, ok := pc.Annotations["assume-role"]; ok {
+			externalId, ok := pc.Annotations["external-id"]
+			if !ok {
+				return nil, errors.New("external-id must be set when assume-role is set")
+			}
+			cfg, err := UsePodServiceAccount(ctx, []byte{}, DefaultSection, region)
+			if err != nil {
+				return nil, errors.Wrap(err, "cannot use pod service account")
+			}
+			stsSvc := sts.New(*cfg)
+			sess := strconv.FormatInt(time.Now().UnixNano(), 10)
+			resp, err := stsSvc.AssumeRoleRequest(&sts.AssumeRoleInput{
+				ExternalId:      &externalId,
+				RoleArn:         &role,
+				RoleSessionName: &sess,
+			}).Send(ctx)
+			if err != nil {
+				return nil, err
+			}
+			creds := credentials.NewStaticCredentials(
+				aws.StringValue(resp.Credentials.AccessKeyId),
+				aws.StringValue(resp.Credentials.SecretAccessKey),
+				aws.StringValue(resp.Credentials.SessionToken))
+
+			config := SetResolverV1(ctx, mg, awsv1.NewConfig().WithCredentials(creds).WithRegion(region))
+			return session.NewSession(config)
+		}
 		cfg, err := UsePodServiceAccountV1(ctx, []byte{}, mg, DefaultSection, region)
 		if err != nil {
 			return nil, errors.Wrap(err, "cannot use pod service account")
