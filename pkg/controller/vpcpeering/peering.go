04e52f32f6d1f1c2221bedb21f2d0131c45f1581
zhengjiajin <zhengjiajin@pingcap.com>
2021-12-17 17:00:20 +0800

    Reduce unnecessary AWS API requests

--- pkg/controller/vpcpeering/peering.go
+++ pkg/controller/vpcpeering/peering.go
@@ -178,6 +178,12 @@ func (e *external) Observe(ctx context.Context, mg cpresource.Managed) (managed.
 		}
 	}
 
+	// If vpc peering connection status is pending acceptance, modify vpc peering attributes request will failed.
+	// In order to reduce the API request to AWS, return errors early to avoid unnecessary API requests.
+	if existedPeer.Status.Code == ec2.VpcPeeringConnectionStateReasonCodePendingAcceptance {
+		return managed.ExternalObservation{ResourceExists: true}, fmt.Errorf("Waiting for the user to accept the vpc peering connection")
+	}
+
 	_, routeTableReady := cr.GetAnnotations()[routeTableEnsured]
 	_, hostZoneReady := cr.GetAnnotations()[hostedZoneEnsured]
 	_, attributeReady := cr.GetAnnotations()[attributeModified]
