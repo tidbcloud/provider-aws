9f7e70ad7d994d10aabfc27619f8058f7ff882dc
Bo Liu <liubo02@pingcap.com>
2025-04-09 18:33:16 +0800

    fix(role): fix diff of assume role policy document (#58)
    Signed-off-by: liubo02 <liubo02@pingcap.com>

--- pkg/utils/policy/compare.go
+++ pkg/utils/policy/compare.go
@@ -1,6 +1,8 @@
 package policy
 
 import (
+	"net/url"
+
 	"github.com/google/go-cmp/cmp"
 )
 
@@ -9,4 +11,26 @@ import (
 func ArePoliciesEqal(a, b *Policy) (equal bool, diff string) {
 	diff = cmp.Diff(a, b)
 	return diff == "", diff
+}
+
+func IsPolicyDocumentUpToDate(a, b string) (bool, string, error) {
+	ua, err := url.QueryUnescape(a)
+	if err != nil {
+		return false, "", err
+	}
+	ub, err := url.QueryUnescape(b)
+	if err != nil {
+		return false, "", err
+	}
+	pa, err := ParsePolicyString(ua)
+	if err != nil {
+		return false, "", err
+	}
+	pb, err := ParsePolicyString(ub)
+	if err != nil {
+		return false, "", err
+	}
+
+	areEqual, diff := ArePoliciesEqal(&pa, &pb)
+	return areEqual, diff, nil
 }
